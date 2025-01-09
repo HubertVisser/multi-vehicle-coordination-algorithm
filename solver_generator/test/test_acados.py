c3dcdc9a9cb4a04636e55bf150a8dd1ddabeb4f8
HubertVisser <80408959+HubertVisser@users.noreply.github.com>
2025-01-09 14:54:21 +0100

    test added

--- solver_generator/test/test_acados.py
+++ solver_generator/test/test_acados.py
@@ -1,41 +1,44 @@
 import sys, os
 
+sys.path.append(os.path.join(sys.path[0], ".."))
+sys.path.append(os.path.join(sys.path[0], "..", "..", "mpc_planner_modules", "scripts"))
+
 import numpy as np
 
-from solver_generator.util.files import load_test_settings, get_current_package
-from solver_generator.util.parameters import Parameters
+from util.files import load_test_settings, get_current_package
+from util.parameters import Parameters
 
-from solver_generator.solver_model import BicycleModel2ndOrder
+from solver_model import ContouringSecondOrderUnicycleModel
 
-from solver_generator.generate_solver import generate_solver
+from generate_solver import generate_solver
 
-from solver_generator.control_modules import ModuleManager, ObjectiveModule, ConstraintModule
-from mpc_planner_modules.mpc_base import MPCBaseModule
-from mpc_planner_modules.contouring import ContouringModule
-from mpc_planner_modules.goal_module import GoalModule
-from mpc_planner_modules.path_reference_velocity import PathReferenceVelocityModule
+from control_modules import ModuleManager, ObjectiveModule, ConstraintModule
+from mpc_base import MPCBaseModule
+from contouring import ContouringModule
+from goal_module import GoalModule
+from path_reference_velocity import PathReferenceVelocityModule
 
-from mpc_planner_modules.ellipsoid_constraints import EllipsoidConstraintModule
-from mpc_planner_modules.gaussian_constraints import GaussianConstraintModule
-from mpc_planner_modules.guidance_constraints import GuidanceConstraintModule
-from mpc_planner_modules.linearized_constraints import LinearizedConstraintModule
-from mpc_planner_modules.scenario_constraints import ScenarioConstraintModule
+from ellipsoid_constraints import EllipsoidConstraintModule
+from gaussian_constraints import GaussianConstraintModule
+from guidance_constraints import GuidanceConstraintModule
+from linearized_constraints import LinearizedConstraintModule
+from scenario_constraints import ScenarioConstraintModule
 
 # from solver_definition import define_parameters, objective, constraints, constraint_lower_bounds, constraint_upper_bounds, constraint_number
 
 
 def solver_configuration(settings):
     modules = ModuleManager()
-    model = BicycleModel2ndOrder()
+    model = ContouringSecondOrderUnicycleModel()
 
     # Penalize ||a||_2^2 and ||w||_2^2
     base_module = modules.add_module(MPCBaseModule(settings))
     base_module.weigh_variable(var_name="a", weight_names="acceleration")
     base_module.weigh_variable(var_name="w", weight_names="angular_velocity")
 
-    modules.add_module(ContouringModule(settings))
+    modules.add_module(ContouringModule(settings, num_segments=settings["contouring"]["num_segments"]))
 
-    modules.add_module(PathReferenceVelocityModule(settings))
+    modules.add_module(PathReferenceVelocityModule(settings, num_segments=settings["contouring"]["num_segments"]))
 
     modules.add_module(EllipsoidConstraintModule(settings))
 
@@ -52,7 +55,6 @@ def test_acados_solver_generation():
     settings["integrator_step"] = 0.2
     settings["contouring"] = dict()
     settings["contouring"]["num_segments"] = 8
-    settings["contouring"]["dynamic_velocity_reference"] = False
 
     solver_settings = settings["solver_settings"]
     solver_settings["solver"] = "acados"
