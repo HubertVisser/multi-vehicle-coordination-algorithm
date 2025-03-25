9b5996cb7ac388fe145bd02faefb336a146dd198
Hubert Visser <hubertv16@live.nl>
2025-03-25 16:11:39 +0000

    renaming

---
+++ coordination_/scripts/helpers.py
@@ -0,0 +1,9 @@
+import os, sys
+import pathlib
+
+def get_solver_import_paths():
+    path = pathlib.Path(__file__).parent.resolve()
+    sys.path.append(os.path.join(path, "..", "solver_generator"))
+    sys.path.append(os.path.join(path))
+
+
