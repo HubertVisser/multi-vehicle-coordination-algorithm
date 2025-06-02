721e86c7916022e971602ebbc3c8a191fe873aeb
HubertVisser <80408959+HubertVisser@users.noreply.github.com>
2025-06-02 11:50:22 +0000

    polygon s_norm functionality added

--- mpc_planner_modules/s_2_norm_constraints.py
+++ mpc_planner_modules/s_2_norm_constraints.py
@@ -21,17 +21,19 @@ class s2normConstraintModule(ConstraintModule):
         self.module_name = f"s_2norm"  # c++ name of the module
         self.import_name = "s_2norm.h"
 
-        self.constraints.append(s2normConstraintConstraints(n_robots=settings["number_of_robots"], idx=idx))
+        self.constraints.append(s2normConstraintConstraints(settings, idx=idx))
         self.description = "Bounds on the 2-norm of the s in the dual formulation of the polytopic constraints"
 
 
 # Constraints of the form sqrt(sum(s^2)) =< 1
 class s2normConstraintConstraints:
 
-    def __init__(self, n_robots, idx):
-        self.n_robots = n_robots
+    def __init__(self, settings, idx):
+        self.number_of_robots = settings["number_of_robots"]
         self.idx = idx
-        self.n_constraints = (n_robots-1)*2
+        self.scheme = settings["scheme"]
+        self.n_sides = 8
+        self.n_constraints = (len(self.neighbour_range()) - 1) * 2 # * self.n_sides #2
         self.nh = self.n_constraints
 
     def define_parameters(self, params):
@@ -50,11 +52,17 @@ class s2normConstraintConstraints:
             # upper_bound.append(1)
         return upper_bound
 
+    def neighbour_range(self):
+        if self.scheme == 'distributed':
+            return range(1, self.number_of_robots+1)
+        elif self.scheme == 'centralised':
+            return range(self.idx, self.number_of_robots+1)
+        
     def get_constraints(self, model, params, settings, stage_idx):
         constraints = []
 
-        for j in range(1, self.n_robots+1): 
-            if j== self.idx:
+        for j in range(1, self.number_of_robots+1): 
+            if j == self.idx:
                 continue
 
             if self.idx < j:
@@ -63,6 +71,11 @@ class s2normConstraintConstraints:
             else:
                 s_ij_0 = model.get(f"s_{j}_{self.idx}_0")
                 s_ij_1 = model.get(f"s_{j}_{self.idx}_1")
+
+            # angles = np.linspace(0, 2*np.pi, self.n_sides, endpoint=False)
+            # A = np.column_stack((np.cos(angles), np.sin(angles)))
+            # for k in range(self.n_sides):
+                # constraints.append(A[k, 0] * s_ij_0 + A[k, 1] * s_ij_1)
 
             constraints.append(cd.norm_2(s_ij_0))
             constraints.append(cd.norm_2(s_ij_1))
